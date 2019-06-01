8a2a90877f2f35a82f32811de51eee3bad8fd65b
毛庭峰 <mtingfeng@gmail.com>
2019-06-01 21:45:45 +0800

    fix 兼容*/*的情况，实际浏览器不会发送*/*的accept

--- src/util/transfer2accept.ts
+++ src/util/transfer2accept.ts
@@ -1,26 +1,34 @@
 import { Context } from "koa"
+import findModuleIdWithRequestKey from "./find-moduleid-with-requestkey"
 
-const html = "html" ,
-    json = "json" ,
-    image = "image" ,
-    text = "text" ,
+const json = "application/json" ,
+    image = "image/*" ,
+    html = "text/html" ,
+    text = "text/plain" ,
     supportedAccept = [ json , html , text , image ]
 
 
 export default async function transfer2Accept( ctx: Context , value: any ) {
-    const accept = ctx.accepts( ...supportedAccept )
+    const accept = ctx.accepts( supportedAccept )
+
     switch ( accept ) {
         case json:
             ctx.type = accept
-            ctx.body = value
+            ctx.body = ( value as object )
             break
         case text:
         case html:
             ctx.type = accept
             ctx.body = typeof value === "object" ? JSON.stringify( value ) : String( value )
             break
+        case image: {
+            const key = `${ctx.method} ${ctx.path}` ,
+                moduleId = await findModuleIdWithRequestKey( key )
+            // console.log( moduleId )
+            break
+        }
         case false:
-            ctx.throw( 406 , `Amock，暂支持${supportedAccept.join(",")}only` )
+            ctx.throw( 406 , `Amock，仅支持${supportedAccept.join(",")}` )
             break
     }
 }
